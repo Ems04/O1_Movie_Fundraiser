1941d14e1a288840aab02d48b9b76f21b552e0b6
smith.eliza <smith.eliza@awatapu.school.nz>
2021-07-07 12:47:33 +1200

    In the process of lay out the formmat of my final code by commenting

---
+++ 00_full_movie_fundraiser_base_component.py
@@ -0,0 +1,10 @@
+# Import statements
+# Functions go here
+# ***** Main Routine *****
+# Get up dictionaries / lists needed to hold data
+# Ask user if they have used the program before
+# Loop to get ticket detail
+    # Name (can't be blank)
+    # Age (between 12 & 130)
+    # Calculate ticket price
+    # Ask for paymethod (
