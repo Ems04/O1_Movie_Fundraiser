d5bd0c0e63558c3ecf7e11a57736360dcd9a15e9
Ems04 <smith.eliza@awatapu.school.nz>
2021-09-09 10:06:59 +1200

    Fixing up code

--- 02_get_input_v2.py
+++ 02_get_input_v2.py
@@ -6,11 +6,19 @@ count = 0
 MAX_TICKETS = 5
 
 while name != 'xxx' and count < MAX_TICKETS:
-    print("You have {} seats left.".format(MAX_TICKETS - count))
+
+    # Tells user how many seats are left
+    if count < 4:
+        print("You have {} seats left.".format(MAX_TICKETS - count))
+
+   # Warns user that only one seat is left
+    else:
+        print("Warning, there is ONE seat left!")
 
     # Get details...
     name = input("Name: ")
     count += 1
+    print()
 
 if count == MAX_TICKETS:
     print("You have sold all the available tickets!")
