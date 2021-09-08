ca906d0584ac814f5004d7e396ad486395ef9773
Ems04 <smith.eliza@awatapu.school.nz>
2021-09-09 10:04:06 +1200

    Put in component togetter

--- 00_full_movie_fundraiser_base_component.py
+++ 00_full_movie_fundraiser_base_component.py
@@ -7,10 +7,10 @@ def not_blank(question):
     while not valid:
         response = input(question)
 
-        # If name is not blank, program continues
+    # If name is not blank, program continues
         if response != "":
             return response
-        # If name if blank, show error (& repeat loop)
+    # If name if blank, show error (& repeat loop)
         else:
             print("This can't be blank, please enter your name.")
 # ***** Main Routine *****
@@ -18,23 +18,24 @@ def not_blank(question):
 # Ask user if they have used the program before
 # Loop to get ticket detail
     # initialise loop so that it runs at least once
-    name = ""
-    count = 0
-    MAX_TICKETS = 5
+name = ""
+count = 0
+MAX_TICKETS = 5
 
-    while name != 'xxx' and count < MAX_TICKETS:
-        print("You have {} seats left.".format(MAX_TICKETS - count))
+while name != 'xxx' and count < MAX_TICKETS:
+    print("You have {} seats left.".format(MAX_TICKETS - count))
 
-        # Get details...
+    # Get details...
 
-        # Name (can't be blank)
-        name = not_blank("Name: ")
-        count += 1
+    # Name (can't be blank)
+    name = not_blank("Name: ")
+    count += 1
+    print()
 
-    if count == MAX_TICKETS:
-        print("You have sold all the available tickets!")
-    else:
-        print("You have sold {} tickets. There are {} palce still available.".format(count, MAX_TICKETS - count))
+if count == MAX_TICKETS:
+    print("You have sold all the available tickets!")
+else:
+    print("You have sold {} tickets. There are {} palce still available.".format(count, MAX_TICKETS - count))
 
     # Age (between 12 & 130)
     # Calculate ticket price
